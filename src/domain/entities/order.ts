16a5f41e7ddcda7b1442ea35893efe43363393db
Salinas <matheussalinas3@gmail.com>
2023-12-15 12:14:26 -0300

    chore: maps relationships

--- src/domain/entities/order.ts
+++ src/domain/entities/order.ts
@@ -1,7 +1,11 @@
 export class Order {
   public id: string
+  public recipientId: string
+  public deliverymanId: string
 
-  constructor(id: string) {
+  constructor(id: string, recipientId: string, deliverymanId: string) {
     this.id = id
+    this.recipientId = recipientId
+    this.deliverymanId = deliverymanId
   }
 }
