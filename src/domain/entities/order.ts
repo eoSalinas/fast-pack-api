848f9d2f0ba6a26c98f1cb1c6b3fe158ad1bdb30
Salinas <matheussalinas3@gmail.com>
2023-12-15 13:20:47 -0300

    chore: map order properties

--- src/domain/entities/order.ts
+++ src/domain/entities/order.ts
@@ -1,8 +1,14 @@
 import { Entity } from '../../core/entities/entity'
+import { UniqueEntityID } from '../../core/entities/unique-entity-id'
 
 interface OrderProps {
-  recipientId: string
-  deliverymanId: string
+  recipientId: UniqueEntityID
+  deliverymanId: UniqueEntityID
+  createdAt: Date
+  availablePickupAt?: Date
+  withdrawedAt?: Date
+  deliveredAt?: Date
+  returnedAt?: Date
 }
 
 export class Order extends Entity<OrderProps> {}
