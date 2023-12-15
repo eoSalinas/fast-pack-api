5709f44e1ca046ff5d322ccf17df917a953813cf
Salinas <matheussalinas3@gmail.com>
2023-12-15 18:54:36 -0300

    chore: add deliveryman properties

--- src/domain/enterprise/entities/deliveryman.ts
+++ src/domain/enterprise/entities/deliveryman.ts
@@ -3,11 +3,21 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 interface DeliverymanProps {
   name: string
+  cpf: string
+  password: string
 }
 
 export class Deliveryman extends Entity<DeliverymanProps> {
   get name() {
     return this.props.name
+  }
+
+  get cpf() {
+    return this.props.cpf
+  }
+
+  get password() {
+    return this.props.password
   }
 
   static create(props: DeliverymanProps, id?: UniqueEntityID) {
