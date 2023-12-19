be8ed681266e42865d1323fcd9f21e20a92cca24
Salinas <matheussalinas3@gmail.com>
2023-12-18 21:54:58 -0300

    test: add deliveryman already exist CPF test

--- src/domain/application/use-cases/register-deliveryman.spec.ts
+++ src/domain/application/use-cases/register-deliveryman.spec.ts
@@ -1,3 +1,4 @@
+import { makeDeliveryman } from '@/domain/test/factories/make-deliveryman'
 import { InMemoryDeliverymenRepository } from '@/domain/test/repositories/in-memory-deliverymen-repository'
 import { compare } from 'bcrypt'
 import { RegisterDeliverymanUseCase } from './register-deliveryman'
@@ -33,5 +34,19 @@ describe('Register Deliveryman', () => {
     const isPassowordHashed = await compare('123456', deliveryman.password)
 
     expect(isPassowordHashed).toBeTruthy()
+  })
+
+  it('should not be able to register a deliveryman with CPF already existing', async () => {
+    const newDeliveryman = makeDeliveryman({
+      cpf: '12312312322',
+    })
+
+    inMemoryDeliverymenRepository.items.push(newDeliveryman)
+
+    await expect(() => sut.execute({
+      name: 'John Doe',
+      cpf: '12312312322',
+      password: '123456',
+    })).rejects.toBeInstanceOf(Error)
   })
 })
